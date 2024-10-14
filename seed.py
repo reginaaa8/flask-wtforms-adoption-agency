33c9cbdfee35325c509c9ea81d6496fe94325e0b
Regina Steffen <reginasteffen@live.com>
2024-10-13 22:06:45 -0500

    edited one pet to not be available

--- seed.py
+++ seed.py
@@ -32,7 +32,8 @@ perry = Pet(name="Perry",
                species="porcupine", 
                photo_url="https://images.unsplash.com/photo-1639501483504-0db53233a4d5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cG9yY3VwaW5lfGVufDB8fDB8fHww", 
                age=1, 
-               notes="Food motivated - will do anything for a treat.")
+               notes="Food motivated - will do anything for a treat.", 
+               available=False)
 
 db.session.add(sparkles)
 db.session.add(buddy)
